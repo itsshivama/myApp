31f5bf67159a0a517df1983df8e984b7a8b987a7
Shivam Agrawal <shivam96306@gmail.com>
2023-08-09 10:32:12 +0530

    Formatting

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -11,7 +11,7 @@ export class AssignmentsComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length+1);
+    this.log.push(this.log.length+1);
     this.log.push(new Date());
   }
 
